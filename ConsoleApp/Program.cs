d8298029f910504c62f5c8f08e7bbee627ec34fe
nathanb <nathancbaker@yahoo.com>
2015-08-10 16:57:36 -0500

    adding console app for other testing

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgrammingProblems.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> someDictionary = new Dictionary<int, int>();
+            
+            List<string> friendCourses = new List<string>() { "3", "2", "1", "3", "1", "1" };
+            Dictionary<string, int> recs = new Dictionary<string, int>();
+            foreach(string course in friendCourses)
+                if(recs.Count(c => c.Key == course) == 0)
+                    recs.Add(course, 1);
+                else 
+                    recs[course]++;
+
+            List<KeyValuePair<string, int>> myList = recs.ToList();
+            myList.Sort((firstPair,nextPair) =>
+            {
+                return nextPair.Value.CompareTo(firstPair.Value);
+            });
+            List<string> recomendations = new List<string>();
+            foreach (var i in myList)
+                recomendations.Add(i.Key);
+        }
+    }
+}
